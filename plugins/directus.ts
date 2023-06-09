7758d2586da3c1de0fd8e459f710c891fa3a238b
bryantgillespie <hey@bryantgillespie.com>
2023-06-09 10:29:04 -0400

    plugin formatter

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,21 +1,21 @@
-import { Directus } from '@directus/sdk'
+import { Directus } from '@directus/sdk';
 // Make sure you review the Directus SDK documentation for more information
 // https://docs.directus.io/reference/sdk.html
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-  const { directusUrl, directusToken } = useRuntimeConfig()
+	const { directusUrl, directusToken } = useRuntimeConfig();
 
-  // Create a new instance of the SDK
-  const directus = new Directus(directusUrl, {
-    auth: {
-      staticToken: directusToken,
-    },
-  })
+	// Create a new instance of the SDK
+	const directus = new Directus(directusUrl, {
+		auth: {
+			staticToken: directusToken,
+		},
+	});
 
-  // Inject the SDK into the Nuxt app
-  // Can be accessed from anywhere in the app using $directus
-  // Ex:
-  // const { $directus } = useNuxtApp()
-  //
-  nuxtApp.provide('directus', directus)
-})
+	// Inject the SDK into the Nuxt app
+	// Can be accessed from anywhere in the app using $directus
+	// Ex:
+	// const { $directus } = useNuxtApp()
+	//
+	nuxtApp.provide('directus', directus);
+});
