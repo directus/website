5e90890625844a5e23add71528d1b8dc0ed8e79c
bryantgillespie <hey@bryantgillespie.com>
2023-06-09 08:28:31 -0400

    start of sitemap

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,19 @@
+import { SitemapStream, streamToPromise } from 'sitemap';
+
+export default defineEventHandler(async (event) => {
+	// TODO: Interate over all collections which have routes in Directus and add them to the sitemap.
+
+	const sitemap = new SitemapStream({
+		hostname: 'https://example.com',
+	});
+
+	for (const doc of docs) {
+		sitemap.write({
+			url: doc._path,
+			changefreq: doc.seo.sitemap_change_frequency,
+		});
+	}
+	sitemap.end();
+
+	return streamToPromise(sitemap);
+});
