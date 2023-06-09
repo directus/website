72be280742996dd8ed9f196b9e0d5bc349bd2008
bryantgillespie <hey@bryantgillespie.com>
2023-06-09 10:20:41 -0400

    env fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+
+	runtimeConfig: {
+		public: {
+			directusUrl: process.env.DIRECTUS_URL,
+		},
+		directusToken: process.env.DIRECTUS_TOKEN,
+	},
 });
