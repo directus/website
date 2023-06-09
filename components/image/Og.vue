8c26190357dbb43494f0e2573b145c74d1289ee9
bryantgillespie <hey@bryantgillespie.com>
2023-06-09 09:32:30 -0400

    start of og

---
+++ components/image/Og.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps({
+	imageUrl: { type: String, default: '' },
+	title: { type: String, default: '' },
+	summary: { type: String, default: '' },
+	authorName: { type: String, default: '' },
+	authorImage: { type: String, default: '' },
+	readTime: { type: String, default: '' },
+	badgeLabel: { type: String, default: '' },
+	badgeColor: { type: String, default: '' },
+});
+</script>
+<template>
+	<div>Template for OG Image</div>
+</template>
