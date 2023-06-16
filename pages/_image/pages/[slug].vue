5820181bb5d82bedbf5eb4eb8ca8dccf19263439
Bryant Gillespie <hey@bryantgillespie.com>
2023-06-16 13:40:56 -0400

    Start of Dynamic OG (#5)
    * start of og
    
    * fix props
    
    * add blank layout for og image

---
+++ pages/_image/pages/[slug].vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: 'blank',
+});
+// Import the $directus plugin
+const { $directus } = useNuxtApp();
+
+// Get the params from the Nuxt route
+const { params, path, query } = useRoute();
+
+// Fetch the page data from the Directus API using the Nuxt useAsyncData composable
+// https://v3.nuxtjs.org/docs/usage/data-fetching#useasyncdata
+const {
+	data: page,
+	pending,
+	error,
+} = await useAsyncData(
+	path,
+	() => {
+		return $directus.items('pages').readByQuery({
+			filter: {
+				slug: { _eq: params.slug },
+			},
+			limit: 1,
+			fields: ['*'],
+		});
+	},
+	{
+		transform: (data) => data.data[0],
+		pick: ['title'],
+	}
+);
+
+const type = query.type ?? 'og';
+</script>
+<template>
+	<ImageOg
+		v-if="type === 'og'"
+		:image-url="fileUrl(page.image)"
+		:title="page.title"
+		:summary="page.summary"
+		:read-time="calculateReadTime(page.content)"
+		:badge-label="page.category.title"
+		:badge-color="page.category.color"
+	/>
+</template>
