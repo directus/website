f86c2337d730f2659514866a1de3f0059dc09ae4
Rijk van Zanten <rijkvanzanten@me.com>
2023-06-21 07:16:54 -0400

    Add BaseIcon (#11)
    * Start on base-icon
    
    * Finalize base-icon
    
    * Tweak class name

---
+++ components/src/base-icon/base-icon.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { computed, unref } from 'vue';
+
+export interface BaseIconProps {
+	name: string;
+	size?: 'small' | 'medium' | 'large';
+	weight?: number;
+}
+
+const props = withDefaults(defineProps<BaseIconProps>(), {
+	size: 'medium',
+	weight: 500,
+});
+
+const opticalSize = computed(() => {
+	switch (props.size) {
+		case 'small':
+			return 20;
+		case 'medium':
+		default:
+			return 24;
+		case 'large':
+			return 48;
+	}
+});
+
+const fontSize = computed(() => unref(opticalSize) + 'px');
+</script>
+
+<template>
+	<span class="base-icon" :class="size">{{ name }}</span>
+</template>
+
+<style scoped>
+.base-icon {
+	--base-icon-color: var(--foreground);
+
+	color: var(--base-icon-color);
+	font-family: 'Material Symbols Outlined';
+	font-weight: normal;
+	font-style: normal;
+	display: inline-block;
+	line-height: 1;
+	text-transform: none;
+	letter-spacing: normal;
+	word-wrap: normal;
+	white-space: nowrap;
+	direction: ltr;
+	font-size: v-bind(fontSize);
+	font-variation-settings: 'opsz' v-bind(opticalSize), 'wght' v-bind(weight);
+	user-select: none;
+}
+</style>
