806087d4f275933c7d04213fc611245497ac79a7
rijkvanzanten <rijkvanzanten@me.com>
2023-06-21 16:07:26 -0400

    Rename config js to cjs

--- components/docgen.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	componentsRoot: 'src', // the folder where CLI will start searching for components.
-	outDir: 'src', // folder to save components docs in (relative to the current working directry)
-	getDocFileName: (componentPath: string) => componentPath.replace(/\.vue$/, '.md'), // specify the name of the input md file
-	getDestFile: (file: string, config: DocgenCLIConfig) => path.join(config.outDir, file).replace(/\.vue$/, '.doc.md'), // specify the name of the output md file
-};
