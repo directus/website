7aed2fc45303b0aa60af30feedb965cda1b9b838
rijkvanzanten <rijkvanzanten@me.com>
2023-06-21 15:26:54 -0400

    Add base-heading component

--- components/src/base-heading/base-heading.story.vue
+++ components/src/base-heading/base-heading.story.vue
@@ -1,5 +1,30 @@
-<template>
-	<Story title="Base / Heading">
+<script setup lang="ts">
+import { reactive } from 'vue';
+import BaseHeading from './base-heading.vue';
 
+const state = reactive({
+	size: 'large',
+	tag: 'h2',
+	icon: undefined,
+});
+</script>
+
+<template>
+	<Story title="Base / Heading / Dynamic" auto-props-disabled>
+		<template #controls>
+			<HstSelect
+				v-model="state.size"
+				title="Size"
+				:options="{
+					small: 'Small',
+					medium: 'Medium',
+					large: 'Large',
+					title: 'Title',
+				}"
+			/>
+			<HstText v-model="state.icon" title="Icon" />
+		</template>
+
+		<BaseHeading :size="state.size" :icon="state.icon">The quick brown fox jumps over the lazy dog</BaseHeading>
 	</Story>
 </template>
--- components/src/base-heading/base-heading.vue
+++ components/src/base-heading/base-heading.vue
@@ -1,16 +1,72 @@
 <script setup lang="ts">
+import { computed, toRefs, unref } from 'vue';
+import BaseIcon from '../base-icon/base-icon.vue';
+
 export interface BaseHeadingProps {
-	size?: 'small' | 'medium' | 'large';
+	size?: 'title' | 'large' | 'medium' | 'small';
+	tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'strong';
+	icon?: string;
 }
 
 const props = withDefaults(defineProps<BaseHeadingProps>(), {
-	size: 'medium',
+	size: 'large',
+	tag: 'h2',
+});
+
+const { tag, size } = toRefs(props);
+
+const iconSize = computed(() => {
+	return unref(size);
 });
 </script>
 
-<template>x</template>
+<template>
+	<component :is="tag" class="base-heading" :class="size">
+		<BaseIcon v-if="icon && size !== 'title'" :name="icon" :size="iconSize" :weight="700" />
+		<slot />
+	</component>
+</template>
 
 <style scoped>
 .base-heading {
+	font-family: var(--family-display);
+	color: currentColor;
+	font-weight: 600;
+	margin: 0;
+}
+
+.base-icon {
+	vertical-align: middle;
+}
+
+.title {
+	font-size: 96px;
+	line-height: 96px;
+}
+
+.large {
+	font-size: 40px;
+	line-height: 48px;
+}
+
+.medium {
+	font-size: 24px;
+	line-height: 32px;
+}
+
+.medium .base-icon {
+	vertical-align: -4px;
+	margin-right: 4px;
+}
+
+.small {
+	font-size: 14px;
+	line-height: 26px;
+	text-transform: uppercase;
+}
+
+.small .base-icon {
+	vertical-align: -2px;
+	margin-right: 2px;
 }
 </style>
