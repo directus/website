06a3cfd64de277d1ec53c57b019bc4b2f0fc04fe
bryantgillespie <hey@bryantgillespie.com>
2023-07-07 21:43:10 -0400

    wip

---
+++ components/src/base-divider/base-divider.story.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import BaseDivider from './base-divider.vue';
+</script>
+
+<template>
+	<Story title="Base / Divider">
+		<template #controls></template>
+
+		<BaseDivider />
+	</Story>
+</template>
---
+++ components/src/base-divider/base-divider.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed, unref } from 'vue';
+
+export interface BaseDividerProps {}
+
+const props = withDefaults(defineProps<BaseDividerProps>(), {});
+</script>
+
+<template>
+	<div class="base-divider" />
+</template>
+
+<style scoped>
+.base-divider {
+	width: 100%;
+	border-top: 1px solid var(--gray-300);
+}
+</style>
