db0c33f6bf9b0898f9d3e9b30e6734bc15ecd69f
rijkvanzanten <rijkvanzanten@me.com>
2023-07-12 09:47:12 +0200

    Fix linter warnings

--- components/src/index.ts
+++ components/src/index.ts
@@ -1,3 +1,3 @@
-export { default as BaseIcon } from './base-icon/base-icon.vue';
 export { default as BaseBadge } from './base-badge/base-badge.vue';
 export { default as BaseDivider } from './base-button/base-divider.vue';
+export { default as BaseIcon } from './base-icon/base-icon.vue';
