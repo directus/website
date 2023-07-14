686ca26f88e6726582969d9aca4af00188515018
bryantgillespie <hey@bryantgillespie.com>
2023-07-13 21:20:35 -0400

    import css

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	css: ['@directus/website-components/styles'],
 	devtools: { enabled: true },
 	typescript: {
 		typeCheck: true,
