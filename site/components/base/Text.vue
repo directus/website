a37ceb1596ce8bfc372d5b351d0dfab05eefb05c
bryantgillespie <hey@bryantgillespie.com>
2023-07-17 15:20:03 -0400

    text

---
+++ site/components/base/Text.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+export interface BaseTextProps {
+	/**
+	 * The content of the text.
+	 */
+	content: string;
+
+	/**
+	 * The alignment of the text.
+	 */
+	align?: 'start' | 'center' | 'end';
+}
+
+defineProps<BaseTextProps>();
+</script>
+
+<template>
+	<div class="base-text" v-html="content"></div>
+</template>
+
+<style scoped>
+.base-text {
+	font-family: var(--family-body);
+	color: var(--gray-500);
+
+	/* TODO: Add styling for all base elements */
+}
+</style>
