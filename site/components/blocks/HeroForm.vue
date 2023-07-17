2f3b4caa7e2b27c56f5649930f281cbaa755ad73
bryantgillespie <hey@bryantgillespie.com>
2023-07-17 16:10:11 -0400

    split with form

---
+++ site/components/blocks/HeroForm.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { BlockHeroForm } from '~/types';
+
+defineProps<{
+	data: BlockHeroForm;
+}>();
+</script>
+
+<template>
+	<div class="flex">
+		<div>
+			<BaseHeading size="title" :content="data.heading" />
+			<BaseText :content="data.subheading" />
+		</div>
+		<div>
+			{{ data.form }}
+		</div>
+	</div>
+</template>
+
+<style scoped>
+@media (min-width: 768px) {
+	.flex {
+		display: flex;
+	}
+}
+</style>
