91957ceb2ee2f56187c3e08663fceaff228b5046
bryantgillespie <hey@bryantgillespie.com>
2023-07-17 16:09:55 -0400

    rotator (probably not for launch)

---
+++ site/components/blocks/HeroRotator.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { BlockHeroRotator } from '~~/types';
+
+defineProps<{
+	data: BlockHeroRotator;
+}>();
+</script>
+
+<template>
+	<div>BlockHeroRotator</div>
+</template>
