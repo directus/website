2c612903c5311f40159a498014dacc4fcb8f150c
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-07-18 10:13:00 +0200

    Remove outdated styles import

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['@directus/website-components/styles'],
 	devtools: { enabled: true },
 	typescript: {
 		typeCheck: true,
