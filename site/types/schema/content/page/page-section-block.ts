3d4416d4f73409b8b726cc577ae7e025d35a0aea
rijkvanzanten <rijkvanzanten@me.com>
2023-07-19 17:46:06 -0400

    Upgrade block types

--- site/types/schema/content/page/page-section-block.ts
+++ site/types/schema/content/page/page-section-block.ts
@@ -1,12 +1,25 @@
 import type { Block } from './block/index.js';
 import type { PageSection } from './page-section.js';
 
+export type BlockType =
+	| 'block_hero_form'
+	| 'block_hero_headline'
+	| 'block_hero_rotator'
+	| 'block_media_fullwidth'
+	| 'block_separator'
+	| 'block_logocloud'
+	| 'block_pageheader'
+	| 'block_featuregrid'
+	| 'block_columns'
+	| 'block_showcase'
+	| 'block_cardgroup';
+
 export interface PageSectionBlock {
 	id: string;
 	sort: number | null;
 	page_sections_id: string | PageSection | null;
 	item: string | Block | null;
-	collection: string | null;
+	collection: BlockType | null;
 	/** Title is only for internal use. */
 	title: string | null;
 }
