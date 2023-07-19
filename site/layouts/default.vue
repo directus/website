8c0a760f66fb3d09b7cf11f5666830ec860ae535
rijkvanzanten <rijkvanzanten@me.com>
2023-07-19 16:20:44 -0400

    Remove flex default styles

--- site/layouts/default.vue
+++ site/layouts/default.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="layout-container">
-		<TheHeader />
-		<slot />
-		<TheFooter />
-	</div>
+	<TheHeader />
+	<slot />
+	<TheFooter />
 </template>
-
-<style scoped>
-.layout-container {
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	min-height: 100vh;
-	overflow: hidden;
-}
-</style>
