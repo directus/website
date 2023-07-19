b6969bd924c61541d2b2b0536fecc310c905bc2a
rijkvanzanten <rijkvanzanten@me.com>
2023-07-19 17:45:44 -0400

    Fetch permalink data

--- site/pages/[...permalink].vue
+++ site/pages/[...permalink].vue
@@ -1,47 +1,35 @@
 <script setup lang="ts">
-/**
- * @TODO pageBlockFields is not a valid field selector
- */
-// import { Page } from '~~/types/schema';
-// import { pageBlockFields } from '~/page-block-fields';
-// import { readItems } from '@directus/sdk';
+import { readItems } from '@directus/sdk';
 
-// const { $directus } = useNuxtApp();
-// const { path } = useRoute();
+const { $directus } = useNuxtApp();
+const { path } = useRoute();
 
-// const constructPageFilter = (path: string) => {
-// 	const finalPath = path.endsWith('/') ? path.slice(0, -1) : path;
-// 	return { permalink: { _eq: finalPath } };
-// };
+const pageFilter = computed(() => {
+	const finalPath = path.endsWith('/') ? path.slice(0, -1) : path;
+	return { permalink: { _eq: finalPath } };
+});
 
-// const { data: page } = await useAsyncData(
-// 	path,
-// 	() => {
-// 		return $directus.request(
-// 			readItems('pages', {
-// 				filter: constructPageFilter(path),
-// 				fields: pageBlockFields,
-// 			})
-// 		);
-// 	},
-// 	{
-// 		transform: (data) => data[0],
-// 	}
-// );
+const { data: page } = await useAsyncData(
+	path,
+	() => {
+		return $directus.request(
+			readItems('pages', {
+				filter: unref(pageFilter),
+				fields: ['title', { sections: ['id', 'background', { blocks: ['id', 'collection'] }] }],
+				limit: 1,
+			})
+		);
+	},
+	{
+		transform: (data) => data[0],
+	}
+);
 
-// useHead({
-// 	title: computed(() => page.value?.title ?? null),
-// });
+useHead({
+	title: computed(() => unref(page)?.title ?? null),
+});
 </script>
 
 <template>
-	<div>
-		<!-- <PageBuilder :page="page" /> -->
-		<!-- Show JSON data when in dev mode -->
-		<!-- <DevOnly>
-			<div class="">
-				<pre>{{ page }}</pre>
-			</div>
-		</DevOnly> -->
-	</div>
+	<PageBuilder v-if="page" :sections="page.sections" />
 </template>
