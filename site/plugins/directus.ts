00236ae7bb8672df9c3f61f3a01ea6e3f983a53c
bryantgillespie <hey@bryantgillespie.com>
2023-07-19 08:36:13 -0400

    fix plugin config

--- site/plugins/directus.ts
+++ site/plugins/directus.ts
@@ -2,7 +2,8 @@ import { createDirectus, rest, staticToken } from '@directus/sdk';
 import type { Schema } from '~/types/schema';
 
 export default defineNuxtPlugin(() => {
-	const { directusUrl, directusToken } = useRuntimeConfig();
+	const config = useRuntimeConfig();
+	const { directusUrl, directusToken } = config.public;
 
 	const directus = createDirectus<Schema>(directusUrl).with(staticToken(directusToken)).with(rest());
 
