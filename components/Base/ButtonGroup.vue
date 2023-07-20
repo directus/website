ebd710a0e142aabab8f87108aefe2e57e1f9e07b
rijkvanzanten <rijkvanzanten@me.com>
2023-07-20 15:51:26 -0400

    Temp remove invalid styling from buttongroup

--- components/Base/ButtonGroup.vue
+++ components/Base/ButtonGroup.vue
@@ -2,17 +2,24 @@
 	<div class="button-group"><slot /></div>
 </template>
 
-<style>
-.button-group {
-	display: flex;
-	margin-top: 1rem;
-	flex-direction: column;
-	gap: 2rem;
+<style scoped>
+/**
+	@TODO Don't include margins in base components
+	@TODO Use container queries instead of media queries for base components
+*/
 
-	@media (min-width: 768px) {
-		margin-left: 1rem;
+/* .button-group {
+	display: flex;
+	margin-top: var(--space-4);
+	flex-direction: column;
+	gap: var(--space-8);
+}
+
+@media (min-width: 768px) {
+	.button-group {
+		margin-left: var(--space-4);
 		margin-top: 0;
 		flex-direction: row;
 	}
-}
+} */
 </style>
