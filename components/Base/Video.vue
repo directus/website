f838be50579f5f6e744f6f405c1e0ac3292064ce
rijkvanzanten <rijkvanzanten@me.com>
2023-07-20 16:17:10 -0400

    use computed for iframesource

--- components/Base/Video.vue
+++ components/Base/Video.vue
@@ -11,35 +11,42 @@ export interface BaseVideoProps {
 	url: string;
 }
 
-defineProps<BaseVideoProps>();
+const props = defineProps<BaseVideoProps>();
 
-function generateVideoEmbed(url: string) {
-	if (url.includes('youtube.com')) {
+const iframeSrc = computed(() => {
+	const { url } = props;
+
+	if (props.url.includes('youtube.com')) {
 		return `https://www.youtube.com/embed/${url.split('v=')[1]}`;
 	}
-	// Handle youtu.be links
-	else if (url.includes('youtu.be')) {
+
+	if (url.includes('youtu.be')) {
 		return `https://www.youtube.com/embed/${url.split('be/')[1]}`;
-	} else if (url.includes('vimeo.com')) {
-		return `https://player.vimeo.com/video/${url.split('vimeo.com/')[1]}`;
-	} else if (url.includes('loom.com')) {
-		return `https://www.loom.com/embed/${url.split('loom.com/share/')[1]}`;
-	} else {
-		return url;
 	}
-}
+
+	if (url.includes('vimeo.com')) {
+		return `https://player.vimeo.com/video/${url.split('vimeo.com/')[1]}`;
+	}
+
+	if (url.includes('loom.com')) {
+		return `https://www.loom.com/embed/${url.split('loom.com/share/')[1]}`;
+	}
+
+	return null;
+});
 </script>
 
 <template>
 	<iframe
+		v-if="iframeSrc"
 		class="base-video"
 		loading="lazy"
-		:src="generateVideoEmbed(url)"
+		:src="iframeSrc"
 		frameborder="0"
 		allow="autoplay; fullscreen; picture-in-picture"
 		allowfullscreen
 		:title="title"
-	></iframe>
+	/>
 </template>
 
 <style scoped>
