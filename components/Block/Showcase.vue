3553f0551695626e6feca16c88f15dd5e821be16
rijkvanzanten <rijkvanzanten@me.com>
2023-07-24 17:23:21 -0400

    Update data fetching for showcase block

--- components/Block/Showcase.vue
+++ components/Block/Showcase.vue
@@ -1,21 +1,34 @@
 <script setup lang="ts">
-import { BlockShowcase } from '~~/types';
-import { ref, watch, onBeforeUnmount, onMounted } from 'vue';
-
-const { fileUrl } = useFiles();
-
-const props = defineProps<{
-	data: BlockShowcase;
-}>();
+import { readItem } from '@directus/sdk';
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue';
+import type { BlockProps } from './types';
 
 const timerLength = 5000;
 const selectedIdx = ref(0);
 const beenClicked = ref(false);
-let timer = null;
-let observer = null;
+
+const { $directus } = useNuxtApp();
+
+const props = defineProps<BlockProps>();
+
+const { data: block } = useAsyncData(props.uuid, () =>
+	$directus.request(
+		readItem('block_showcase', props.uuid, {
+			fields: [
+				{
+					items: ['heading', 'icon', 'subheading', { image: ['id', 'description'] }],
+				},
+			],
+		})
+	)
+);
+
+let timer: NodeJS.Timeout | undefined = undefined;
+
+let observer: IntersectionObserver | undefined = undefined;
 const showcaseRef = ref(null);
 
-function handleClick(itemIdx: number) {
+const handleClick = (itemIdx: number) => {
 	selectedIdx.value = itemIdx;
 	beenClicked.value = true;
 	clearInterval(timer);
@@ -24,13 +37,13 @@ function handleClick(itemIdx: number) {
 	setTimeout(() => {
 		clearInterval(timer);
 	}, timerLength);
-}
+};
 
-function startTimer() {
+const startTimer = () => {
 	timer = setInterval(() => {
-		selectedIdx.value = (selectedIdx.value + 1) % props.data.items.length;
+		selectedIdx.value = (selectedIdx.value + 1) % (unref(block)?.items.length ?? 0);
 	}, timerLength);
-}
+};
 
 watch(
 	selectedIdx,
@@ -50,7 +63,7 @@ onMounted(() => {
 		threshold: 0.1,
 	};
 
-	observer = new IntersectionObserver((entries, observer) => {
+	observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			if (entry.isIntersecting && !beenClicked.value) {
 				startTimer();
@@ -67,6 +80,7 @@ onMounted(() => {
 
 onBeforeUnmount(() => {
 	clearInterval(timer);
+
 	if (observer && showcaseRef.value) {
 		observer.unobserve(showcaseRef.value);
 	}
@@ -74,20 +88,20 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-	<div class="block-showcase" ref="showcaseRef">
+	<div v-if="block" ref="showcaseRef" class="block-showcase">
 		<!-- Showcase Frame -->
 		<BaseFrame aspect="16-9" variant="frosted" color="white">
 			<div class="transition-group">
-				<template v-for="(item, itemIdx) in data.items" :key="item.id">
+				<template v-for="(item, itemIdx) in block.items" :key="item.id">
 					<div class="image-container" :class="{ 'is-active': selectedIdx === itemIdx }">
-						<img :src="fileUrl(item.image?.id)" :alt="item.image?.alt" />
+						<img :src="getFileUrl(item.image?.id)" :alt="item.image?.description ?? undefined" />
 					</div>
 				</template>
 			</div>
 		</BaseFrame>
 		<!-- Features Buttons -->
 		<div class="showcase-buttons">
-			<template v-for="(item, itemIdx) in data.items" :key="item.id">
+			<template v-for="(item, itemIdx) in block.items" :key="item.id">
 				<button
 					class="showcase-button"
 					:class="[{ 'is-active': selectedIdx === itemIdx }]"
@@ -96,8 +110,8 @@ onBeforeUnmount(() => {
 					<div class="timer-bar" :class="{ 'is-active': selectedIdx === itemIdx }">
 						<span />
 					</div>
-					<BaseHeading font="body" size="medium" :icon="item.icon" :content="item.heading" />
-					<BaseText align="left" :content="item.subheading" />
+					<BaseHeading font="body" size="medium" :icon="item.icon ?? undefined" :content="item.heading" />
+					<BaseText align="start" :content="item.subheading" />
 				</button>
 			</template>
 		</div>
@@ -111,29 +125,30 @@ onBeforeUnmount(() => {
 	grid-template-columns: repeat(3, 1fr);
 	gap: var(--space-4);
 	margin-top: var(--space-4);
+}
 
-	.showcase-button {
-		padding: var(--space-4);
-		display: flex;
-		flex-direction: column;
-		height: 100%;
-		text-align: left;
-		background: none;
-		color: inherit;
-		border: none;
-		border-radius: var(--rounded-lg);
-		transition: background-color 0.5s ease;
-		font: inherit;
-		cursor: pointer;
-		outline: inherit;
-		position: relative;
-		&:hover,
-		&.is-active {
-			background: rgba(255, 255, 255, 0.2);
-			box-shadow: 0px 30px 60px -30px rgba(0, 0, 0, 0.05), 0px 2px 4px 0px rgba(0, 0, 0, 0.05);
-			backdrop-filter: blur(2px);
-		}
-	}
+.showcase-buttons .showcase-button {
+	padding: var(--space-4);
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	text-align: left;
+	background: none;
+	color: inherit;
+	border: none;
+	border-radius: var(--rounded-lg);
+	transition: background-color 0.5s ease;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+	position: relative;
+}
+
+.showcase-buttons .showcase-button:hover,
+.showcase-buttons .showcase-button.is-active {
+	background: rgba(255, 255, 255, 0.2);
+	box-shadow: 0px 30px 60px -30px rgba(0, 0, 0, 0.05), 0px 2px 4px 0px rgba(0, 0, 0, 0.05);
+	backdrop-filter: blur(2px);
 }
 
 .image-container {
