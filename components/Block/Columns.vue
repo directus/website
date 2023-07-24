30291b748ba3d73b0f260d8813051b7574047418
rijkvanzanten <rijkvanzanten@me.com>
2023-07-24 16:35:29 -0400

    Fetch data for columns block

--- components/Block/Columns.vue
+++ components/Block/Columns.vue
@@ -1,23 +1,41 @@
 <script setup lang="ts">
-import { BlockColumns } from '~~/types';
+import { readItem } from '@directus/sdk';
+import { BlockProps } from './types';
 
-defineProps<{
-	data: BlockColumns;
-}>();
+const { $directus } = useNuxtApp();
 
-// Column Component Map
-const map = (collection: string) => {
-	const mapping = {
-		comp_quote: resolveComponent('CompQuote'),
-		comp_heading: resolveComponent('CompHeading'),
-		comp_button_groups: resolveComponent('CompButtonGroup'),
-		comp_metrics: resolveComponent('CompMetrics'),
-		comp_media: resolveComponent('CompMedia'),
-		comp_cards: resolveComponent('CompCards'),
-	};
+const props = defineProps<BlockProps>();
 
-	return mapping[collection] || 'div';
-};
+const { data: block } = useAsyncData(() =>
+	$directus.request(
+		readItem('block_columns', props.uuid, {
+			fields: [
+				{
+					col_one: ['collection', 'item'],
+					col_two: ['collection', 'item'],
+				},
+			],
+		})
+	)
+);
+
+// defineProps<{
+// 	data: BlockColumns;
+// }>();
+
+// // Column Component Map
+// const map = (collection: string) => {
+// 	const mapping = {
+// 		comp_quote: resolveComponent('CompQuote'),
+// 		comp_heading: resolveComponent('CompHeading'),
+// 		comp_button_groups: resolveComponent('CompButtonGroup'),
+// 		comp_metrics: resolveComponent('CompMetrics'),
+// 		comp_media: resolveComponent('CompMedia'),
+// 		comp_cards: resolveComponent('CompCards'),
+// 	};
+
+// 	return mapping[collection] || 'div';
+// };
 </script>
 
 <template>
@@ -31,19 +49,20 @@ const map = (collection: string) => {
 	</div>
 </template>
 
-<style>
+<style scoped>
 .block-columns {
 	display: flex;
 	position: relative;
 	width: 100%;
 
 	gap: var(--space-16);
+}
 
-	.column {
-		width: 50%;
-		> * + * {
-			margin-top: var(--space-8);
-		}
-	}
+.block-columns .column {
+	width: 50%;
+}
+
+.block-columns .column > * + * {
+	margin-top: var(--space-8);
 }
 </style>
