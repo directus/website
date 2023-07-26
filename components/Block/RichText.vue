2a1e1c6f9f574229ea1bf71dfb89cbca3adfeb77
Rijk van Zanten <rijkvanzanten@me.com>
2023-07-26 15:45:48 -0400

    Update components/Block/RichText.vue

--- components/Block/RichText.vue
+++ components/Block/RichText.vue
@@ -5,7 +5,7 @@ const { $directus, $readItem } = useNuxtApp();
 
 const props = defineProps<BlockProps>();
 
-const { data: comp } = useAsyncData(() =>
+const { data: comp } = useAsyncData(props.uuid, () =>
 	$directus.request(
 		$readItem('block_richtext', props.uuid, {
 			fields: ['content'],
