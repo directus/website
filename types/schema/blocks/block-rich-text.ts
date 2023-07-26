07e8b7f08a89d014bca8f8dba1ccf5837cccd557
bryantgillespie <hey@bryantgillespie.com>
2023-07-26 14:48:30 -0400

    major typage

---
+++ types/schema/blocks/block-rich-text.ts
@@ -0,0 +1,4 @@
+export interface BlockRichtext {
+	id: string;
+	content: string;
+}
--- types/schema/schema.ts
+++ types/schema/schema.ts
@@ -7,6 +7,7 @@ import type {
 	BlockMediaFullWidth,
 	BlockPageHeader,
 	BlockShowcase,
+	BlockRichtext,
 } from './blocks/index.js';
 import type {
 	ComponentButton,
@@ -48,6 +49,7 @@ export interface Schema {
 	block_media_fullwidth: BlockMediaFullWidth[];
 	block_showcase: BlockShowcase[];
 	block_pageheader: BlockPageHeader[];
+	block_richtext: BlockRichtext[];
 
 	// Components
 	comp_button_groups: ComponentButtonGroup[];
