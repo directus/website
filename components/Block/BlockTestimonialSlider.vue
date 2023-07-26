e3f28cd4c4b75b28e58370b878e93a1829f6d2a0
bryantgillespie <hey@bryantgillespie.com>
2023-07-26 16:06:48 -0400

    testimonial comp

---
+++ components/Block/BlockTestimonialSlider.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { BlockProps } from './types';
+
+const { $directus, $readItem } = useNuxtApp();
+
+const props = defineProps<BlockProps>();
+
+const { data: comp } = useAsyncData(() =>
+	$directus.request(
+		$readItem('block_testimonial_slider', props.uuid, {
+			fields: [
+				'id',
+				{ items: ['id', 'company_logo', 'company_name', 'person_name', 'person_title', 'person_image', 'quote'] },
+			],
+		})
+	)
+);
+</script>
+
+<template>
+	!
+	<div class="testimonial-slider"></div>
+</template>
