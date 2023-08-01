243d123fedad343f7cf8ae637436570e5a9e3e09
rijkvanzanten <rijkvanzanten@me.com>
2023-08-01 18:42:19 -0400

    Use directusimage in media full width

--- components/Block/MediaFullWidth.vue
+++ components/Block/MediaFullWidth.vue
@@ -8,24 +8,22 @@ const props = defineProps<BlockProps>();
 const { data: block } = useAsyncData(props.uuid, () =>
 	$directus.request(
 		$readItem('block_media_fullwidth', props.uuid, {
-			fields: ['type', 'embed', { video: ['url'], image: ['id', 'title'] }],
+			fields: ['type', 'embed', { video: ['url'], image: ['id', 'description'] }],
 		})
 	)
 );
-
-const imgSrc = computed(() => {
-	const type = unref(block)?.type;
-	const imageId = unref(block)?.image?.id;
-	if (type !== 'image' || !imageId) return null;
-
-	return getFileUrl(imageId);
-});
 </script>
 
 <template>
 	<BaseFrame v-if="block" aspect="16-9" variant="frosted" color="white">
 		<BaseVideo v-if="block.type === 'video' && block.video" :url="block.video.url!" />
-		<img v-else-if="block.type === 'image' && block.image" :src="imgSrc!" :alt="block.image.title" />
+
+		<BaseDirectusImage
+			v-else-if="block.type === 'image' && block.image"
+			:uuid="block.image.id"
+			:alt="block.image.description!"
+		/>
+
 		<!-- eslint-disable-next-line vue/no-v-html -->
 		<div v-else-if="block.type === 'embed' && block.embed" v-html="block.embed" />
 	</BaseFrame>
