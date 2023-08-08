d8ab9e6d576853d3a2108fe04b541e4878b82b7e
rijkvanzanten <rijkvanzanten@me.com>
2023-08-08 15:19:36 -0400

    Add makeshift queueing

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,11 +1,25 @@
-import { createDirectus, readItem, readItems, rest, staticToken, readSingleton } from '@directus/sdk';
+import { createDirectus, readItem, readItems, readSingleton, rest, staticToken } from '@directus/sdk';
+import Queue from 'p-queue';
 import type { Schema } from '~/types/schema';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const queue = new Queue({ concurrency: 10, interval: 1000 });
 
 export default defineNuxtPlugin(() => {
 	const config = useRuntimeConfig();
 	const { directusUrl, directusToken } = config.public;
 
-	const directus = createDirectus<Schema>(directusUrl).with(staticToken(directusToken)).with(rest());
+	const directus = createDirectus<Schema>(directusUrl)
+		.with(staticToken(directusToken))
+		.with(
+			rest({
+				onRequest: async (request) => {
+					await queue.add(() => sleep(500));
+					return request;
+				},
+			})
+		);
 
 	return { provide: { directus, readItem, readItems, readSingleton } };
 });
