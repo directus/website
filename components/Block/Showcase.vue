71c871c9ec734abf9968fccc585c5fc2320cfe9c
Ben Haynes <ben@rngr.org>
2023-08-18 14:55:12 -0400

    showcase cleanup

--- components/Block/Showcase.vue
+++ components/Block/Showcase.vue
@@ -151,11 +151,8 @@ onBeforeUnmount(() => {
 	position: relative;
 }
 
-.showcase-buttons .showcase-button:hover,
-.showcase-buttons .showcase-button.is-active {
-	background: rgba(255, 255, 255, 0.2);
-	box-shadow: 0px 30px 60px -30px rgba(0, 0, 0, 0.05), 0px 2px 4px 0px rgba(0, 0, 0, 0.05);
-	backdrop-filter: blur(2px);
+.showcase-buttons .showcase-button:hover .timer-bar {
+	background-color: var(--gray-300);
 }
 
 .image-container {
@@ -170,7 +167,7 @@ onBeforeUnmount(() => {
 
 .timer-bar {
 	height: 2px;
-	background-color: var(--gray-300);
+	background-color: var(--gray-200);
 	width: 100%;
 	position: relative;
 	margin-bottom: var(--space-2);
@@ -184,14 +181,14 @@ onBeforeUnmount(() => {
 	width: 100%;
 	opacity: 0.5;
 	background-color: var(--primary);
-	transform: scaleX(1);
+	transform: scaleX(0);
 	transform-origin: left;
 	transition: none;
 }
 
 .timer-bar.is-active span {
 	opacity: 1;
-	transform: scaleX(0);
+	transform: scaleX(1);
 	transition: transform 5s linear;
 }
 </style>
