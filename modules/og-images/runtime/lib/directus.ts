5f19df8ce9d221cdae28af48144c07a4f0a5bb2c
bryantgillespie <hey@bryantgillespie.com>
2023-08-24 01:57:13 -0400

    add queue to og image generation

--- modules/og-images/runtime/lib/directus.ts
+++ modules/og-images/runtime/lib/directus.ts
@@ -1,11 +1,25 @@
 import { createDirectus, readItems, readItem, rest, staticToken } from '@directus/sdk';
+import Queue from 'p-queue';
+
 import type { Schema } from '~/types/schema';
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const queue = new Queue({ concurrency: 10, interval: 1000 });
 
 // Fetch the data from Directus
 const directusUrl = process.env.DIRECTUS_URL as string;
 const directusToken = process.env.DIRECTUS_TOKEN as string;
 
-// eslint-disable-next-line
-const directus = createDirectus<Schema>(directusUrl).with(staticToken(directusToken)).with(rest());
+const directus = createDirectus<Schema>(directusUrl)
+	.with(staticToken(directusToken))
+	.with(
+		rest({
+			onRequest: async (request) => {
+				await queue.add(() => sleep(500));
+				return request;
+			},
+		})
+	);
 
 export { directus, readItems, readItem };
