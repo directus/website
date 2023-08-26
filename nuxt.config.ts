8193a974d83c3d27a13348c9c2e68013d1c7045e
Bryant Gillespie <hey@bryantgillespie.com>
2023-08-25 21:06:18 -0400

    Sitemap Module (#50)
    * update env example
    
    * add nuxt simple sitemap module
    
    * add resources pages to prerender
    
    * remove playwright
    
    * fix routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,14 +12,27 @@ const fetchPagePermalinks = async () => {
 		.with(staticToken(process.env.DIRECTUS_TOKEN))
 		.with(rest());
 
-	const permalinks = await directus.request(
+	const permalinks = [];
+
+	const pages = await directus.request(
 		readItems('pages', {
 			fields: ['permalink'],
 			limit: -1,
 		})
 	);
 
-	return permalinks.map((page) => page.permalink);
+	const resources = await directus.request(readItems('resources', { fields: ['slug', { type: ['slug'] }], limit: -1 }));
+
+	permalinks.push(...pages.map((page) => page.permalink));
+
+	permalinks.push(...resources.map((resource) => `/${resource.type.slug}/${resource.slug}`));
+
+	/** @TODO Add team members to prerender routes */
+	// const team = await directus.request(readItems('team', { fields: ['slug'], limit: -1 }));
+
+	// permalinks.push(...team.map((member) => ({ permalink: `/team/${member.slug}` })));
+
+	return permalinks;
 };
 
 export default defineNuxtConfig({
@@ -57,7 +70,10 @@ export default defineNuxtConfig({
 		},
 	},
 
-	modules: ['@vueuse/nuxt'],
+	modules: [
+		'@vueuse/nuxt',
+		'nuxt-simple-sitemap', // https://nuxtseo.com/sitemap/getting-started/how-it-works
+	],
 
 	vite: {
 		vue: {
