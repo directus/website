5b43f045cdec87be2a83d303d336c46845d97fc2
bryantgillespie <hey@bryantgillespie.com>
2023-09-01 15:58:48 -0400

    fix token

--- modules/og-images/runtime/lib/directus.ts
+++ modules/og-images/runtime/lib/directus.ts
@@ -1,4 +1,4 @@
-import { createDirectus, readItems, readItem, rest, staticToken } from '@directus/sdk';
+import { createDirectus, readItems, readItem, rest } from '@directus/sdk';
 import Queue from 'p-queue';
 
 import type { Schema } from '~/types/schema';
@@ -9,17 +9,14 @@ const queue = new Queue({ concurrency: 10, interval: 1000 });
 
 // Fetch the data from Directus
 const directusUrl = process.env.DIRECTUS_URL as string;
-const directusToken = process.env.DIRECTUS_TOKEN as string;
 
-const directus = createDirectus<Schema>(directusUrl)
-	.with(staticToken(directusToken))
-	.with(
-		rest({
-			onRequest: async (request) => {
-				await queue.add(() => sleep(500));
-				return request;
-			},
-		})
-	);
+const directus = createDirectus<Schema>(directusUrl).with(
+	rest({
+		onRequest: async (request) => {
+			await queue.add(() => sleep(500));
+			return request;
+		},
+	})
+);
 
 export { directus, readItems, readItem };
