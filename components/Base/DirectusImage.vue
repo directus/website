2feec10bd1b9cdff529685696126f396b3496397
bryantgillespie <hey@bryantgillespie.com>
2023-10-06 14:36:19 -0400

    swap directus image comp

--- components/Base/DirectusImage.vue
+++ components/Base/DirectusImage.vue
@@ -20,25 +20,27 @@ export interface DirectusImageProps {
 
 const props = defineProps<DirectusImageProps>();
 
-const { pixelRatio } = useDevicePixelRatio();
+// const { pixelRatio } = useDevicePixelRatio();
 
-const src = computed(() => {
-	const url = new URL(`/assets/${props.uuid}`, directusUrl);
+// const src = computed(() => {
+// 	const url = new URL(`/assets/${props.uuid}`, directusUrl);
 
-	url.searchParams.set('format', 'auto');
+// 	url.searchParams.set('format', 'auto');
 
-	if (props.width) {
-		url.searchParams.set('width', String(Math.round(props.width * unref(pixelRatio))));
-	}
+// 	// url.searchParams.set('quality', '100');
 
-	if (props.height) {
-		url.searchParams.set('height', String(Math.round(props.height * unref(pixelRatio))));
-	}
+// 	if (props.width) {
+// 		url.searchParams.set('width', String(Math.round(props.width * unref(pixelRatio))));
+// 	}
 
-	return url.toString();
-});
+// 	if (props.height) {
+// 		url.searchParams.set('height', String(Math.round(props.height * unref(pixelRatio))));
+// 	}
+
+// 	return url.toString();
+// });
 </script>
 
 <template>
-	<img :src="src" :alt="alt" :width="width" :height="height" loading="lazy" />
+	<NuxtImg :src="uuid" :alt="alt" :width="width" :height="height" format="auto" loading="lazy" />
 </template>
