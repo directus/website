74850a44ad516fb636aec89c953d1d82bb6a1211
bryantgillespie <hey@bryantgillespie.com>
2023-10-06 16:11:35 -0400

    remove commented out bits

--- components/Base/DirectusImage.vue
+++ components/Base/DirectusImage.vue
@@ -18,27 +18,7 @@ export interface DirectusImageProps {
 	height?: number;
 }
 
-const props = defineProps<DirectusImageProps>();
-
-// const { pixelRatio } = useDevicePixelRatio();
-
-// const src = computed(() => {
-// 	const url = new URL(`/assets/${props.uuid}`, directusUrl);
-
-// 	url.searchParams.set('format', 'auto');
-
-// 	// url.searchParams.set('quality', '100');
-
-// 	if (props.width) {
-// 		url.searchParams.set('width', String(Math.round(props.width * unref(pixelRatio))));
-// 	}
-
-// 	if (props.height) {
-// 		url.searchParams.set('height', String(Math.round(props.height * unref(pixelRatio))));
-// 	}
-
-// 	return url.toString();
-// });
+defineProps<DirectusImageProps>();
 </script>
 
 <template>
