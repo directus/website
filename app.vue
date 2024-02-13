027aeb585a0d3e8a2d657c4be8ccad9c0efe6dc5
bryantgillespie <hey@bryantgillespie.com>
2024-02-13 13:03:55 -0500

    Remove NavHeader from default layout

--- app.vue
+++ app.vue
@@ -28,24 +28,25 @@ useServerHead({
 	],
 });
 
-// useSchemaOrg([
-// 	defineOrganization({
-// 		name: 'Directus',
-// 		logo: '/images/logo-dark.svg',
-// 		sameAs: [
-// 			'https://hub.docker.com/r/directus/directus',
-// 			'https://twitter.com/directus',
-// 			'https://github.com/directus',
-// 			'https://www.youtube.com/c/DirectusVideos',
-// 			'https://www.linkedin.com/company/directus-io',
-// 			'https://www.npmjs.com/package/directus',
-// 		],
-// 	}),
-// ]);
+useSchemaOrg([
+	defineOrganization({
+		name: 'Directus',
+		logo: '/images/logo-dark.svg',
+		sameAs: [
+			'https://hub.docker.com/r/directus/directus',
+			'https://twitter.com/directus',
+			'https://github.com/directus',
+			'https://www.youtube.com/c/DirectusVideos',
+			'https://www.linkedin.com/company/directus-io',
+			'https://www.npmjs.com/package/directus',
+		],
+	}),
+]);
 </script>
 
 <template>
 	<NuxtLayout>
+		<NavHeader />
 		<NuxtPage />
 	</NuxtLayout>
 </template>
