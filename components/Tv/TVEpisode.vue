73784999794592c0b06dea789e22bea221d303e1
Kevin Lewis <kvn@lws.io>
2024-03-21 09:24:18 +0100

    Added 'episode' in hero and tv listing

--- components/Tv/TVEpisode.vue
+++ components/Tv/TVEpisode.vue
@@ -1,85 +1,85 @@
 <template>
-	<NuxtLink :to="`/tv/${show.slug}/${episode.slug}`" class="episode">
-		<img :src="`${directusUrl}/assets/${episode.tile}?width=600`" alt="" />
-		<div>
-			<h3>
-				<span v-if="!hideNumber">{{ episode.episode_number }}:</span>
-				{{ episode.title }}
-				<span class="meta">
-					<span v-if="episode.length">{{ episode.length }}m</span>
-					<span>{{ formatDate(episode.published) }}</span>
-				</span>
-			</h3>
-			<p>{{ episode.description }}</p>
-		</div>
-	</NuxtLink>
+  <NuxtLink :to="`/tv/${show.slug}/${episode.slug}`" class="episode">
+    <img :src="`${directusUrl}/assets/${episode.tile}?width=600`" alt="" />
+    <div>
+      <h3>
+        <span v-if="!hideNumber">Episode {{ episode.episode_number }}:</span>
+        {{ episode.title }}
+        <span class="meta">
+          <span v-if="episode.length">{{ episode.length }}m</span>
+          <span>{{ formatDate(episode.published) }}</span>
+        </span>
+      </h3>
+      <p>{{ episode.description }}</p>
+    </div>
+  </NuxtLink>
 </template>
 
 <script setup>
 const {
-	public: { tvUrl },
+  public: { tvUrl },
 } = useRuntimeConfig();
 
 const directusUrl = process.env.DIRECTUS_TV_URL || tvUrl;
 
 defineProps({
-	show: Object,
-	episode: Object,
-	hideNumber: Boolean,
+  show: Object,
+  episode: Object,
+  hideNumber: Boolean,
 });
 
 const formatDate = (dateString) => {
-	const formatted = new Intl.DateTimeFormat('en-US', {
-		day: 'numeric',
-		month: 'short',
-		year: 'numeric',
-	})
-		.format(new Date(dateString))
-		.split(',')
-		.join('');
+  const formatted = new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  })
+    .format(new Date(dateString))
+    .split(',')
+    .join('');
 
-	return formatted;
+  return formatted;
 };
 </script>
 
 <style lang="scss" scoped>
 .episode {
-	list-style-type: none;
-	text-decoration: none;
-	color: inherit;
-	display: grid;
-	gap: 1em;
-	img {
-		width: 100%;
-		border-radius: var(--rounded-lg);
-	}
-	h3 .meta {
-		opacity: 0.5;
-		margin-left: 0.5rem;
-		font-size: 0.8rem;
-		span:not(:last-child):after {
-			content: '\2022';
-			margin: 0 0.5em;
-		}
-	}
-	p {
-		margin: 0.5em 0;
-	}
+  list-style-type: none;
+  text-decoration: none;
+  color: inherit;
+  display: grid;
+  gap: 1em;
+  img {
+    width: 100%;
+    border-radius: var(--rounded-lg);
+  }
+  h3 .meta {
+    opacity: 0.5;
+    margin-left: 0.5rem;
+    font-size: 0.8rem;
+    span:not(:last-child):after {
+      content: '\2022';
+      margin: 0 0.5em;
+    }
+  }
+  p {
+    margin: 0.5em 0;
+  }
 }
 
 @media (width > 60rem) {
-	.episode {
-		grid-template-columns: 300px auto;
-	}
+  .episode {
+    grid-template-columns: 300px auto;
+  }
 }
 
 @media (width > 35rem) {
-	.episode {
-		grid-template-columns: 200px auto;
-		gap: 2em;
-	}
-	p {
-		margin: 1em 0;
-	}
+  .episode {
+    grid-template-columns: 200px auto;
+    gap: 2em;
+  }
+  p {
+    margin: 1em 0;
+  }
 }
 </style>
--- pages/tv/index.vue
+++ pages/tv/index.vue
@@ -1,85 +1,85 @@
 <template>
-	<TVHero
-		:cover="globals.featured.season.show.cover"
-		:logo="globals.featured.season.show.logo"
-		:title="globals.featured.title"
-		:description="globals.featured.description"
-		:buttons="heroButtons"
-	/>
-	<BaseContainer class="main">
-		<section class="categories">
-			<TVCategory v-for="category in categories" :key="category.id" :title="category.title" :shows="category.shows" />
-		</section>
-	</BaseContainer>
+  <TVHero
+    :cover="globals.featured.season.show.cover"
+    :logo="globals.featured.season.show.logo"
+    :title="`Episode ${globals.featured.episode_number}: ${globals.featured.title}`"
+    :description="globals.featured.description"
+    :buttons="heroButtons"
+  />
+  <BaseContainer class="main">
+    <section class="categories">
+      <TVCategory v-for="category in categories" :key="category.id" :title="category.title" :shows="category.shows" />
+    </section>
+  </BaseContainer>
 </template>
 
 <script setup>
 import { createDirectus, rest, readItems, readSingleton } from '@directus/sdk';
 
 const {
-	public: { tvUrl, baseUrl },
+  public: { tvUrl, baseUrl },
 } = useRuntimeConfig();
 
 const directusUrl = process.env.DIRECTUS_TV_URL || tvUrl;
 const directus = createDirectus(directusUrl).with(rest());
 
 const globals = await directus.request(
-	readSingleton('globals', { fields: ['og', { featured: ['*', { season: [{ show: ['*'] }] }] }] }),
+  readSingleton('globals', { fields: ['og', { featured: ['*', { season: [{ show: ['*'] }] }] }] }),
 );
 
 const categories = await directus.request(
-	readItems('categories', {
-		fields: ['id', 'title', { shows: [{ shows_id: ['*'] }] }],
-	}),
+  readItems('categories', {
+    fields: ['id', 'title', { shows: [{ shows_id: ['*'] }] }],
+  }),
 );
 
 const heroButtons = [
-	{
-		type: 'primary',
-		icon: 'play_arrow',
-		text: 'Play Episode',
-		href: `/tv/${globals.featured.season.show.slug}/${globals.featured.slug}`,
-	},
-	{
-		type: 'secondary',
-		icon: 'arrow_forward',
-		text: 'Details',
-		href: `/tv/${globals.featured.season.show.slug}`,
-	},
+  {
+    type: 'primary',
+    icon: 'play_arrow',
+    text: 'Play Episode',
+    href: `/tv/${globals.featured.season.show.slug}/${globals.featured.slug}`,
+  },
+  {
+    type: 'secondary',
+    icon: 'arrow_forward',
+    text: 'Details',
+    href: `/tv/${globals.featured.season.show.slug}`,
+  },
 ];
 
 definePageMeta({
-	layout: 'tv',
+  layout: 'tv',
 });
 
 const seoTitle = 'Directus TV';
 const seoDesc = 'Go down the rabbit hole with hours of original video content from the team at Directus.';
 
 useSeoMeta({
-	title: seoTitle,
-	ogTitle: seoTitle,
-	description: seoDesc,
-	ogDescription: seoDesc,
-	ogImage: `${directusUrl}/assets/${globals.og}`,
-	twitterCard: 'summary_large_image',
-	ogUrl: `${baseUrl}/tv`,
+  title: seoTitle,
+  ogTitle: seoTitle,
+  description: seoDesc,
+  ogDescription: seoDesc,
+  ogImage: `${directusUrl}/assets/${globals.og}`,
+  twitterCard: 'summary_large_image',
+  ogUrl: `${baseUrl}/tv`,
 });
 </script>
 
 <style lang="scss" scoped>
 .main {
-	padding-bottom: 8em;
+  padding-bottom: 8em;
 }
 .categories {
-	margin-top: 4rem;
-	display: flex;
-	flex-direction: column;
-	gap: 2rem;
+  margin-top: 4rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
 }
 
 @media (width > 60rem) {
-	.categories {
-		gap: 4rem;
-	}
+  .categories {
+    gap: 4rem;
+  }
 }
 </style>
