ae8ee7053f049dafdb72bfaebbe1a3f0a67f40f3
bryantgillespie <hey@bryantgillespie.com>
2024-04-09 15:54:38 -0400

    cleanup

--- pages/refer/[...refer].vue
+++ pages/refer/[...refer].vue
@@ -2,22 +2,17 @@
 import type { PageBuilderSection } from '~/components/PageBuilder.vue';
 
 const { $directus, $readItems } = useNuxtApp();
-const { path } = useRoute();
 
 const {
 	public: { directusUrl },
 } = useRuntimeConfig();
 
-const pageFilter = computed(() => {
-	return { permalink: { _eq: '/refer' } };
-});
-
 const { data: page } = await useAsyncData(
-	path,
+	'/refer',
 	() => {
 		return $directus.request(
 			$readItems('pages', {
-				filter: unref(pageFilter),
+				filter: { permalink: { _eq: '/refer' } },
 				limit: 1,
 				fields: [
 					'title',
@@ -40,9 +35,9 @@ const { data: page } = await useAsyncData(
 	},
 );
 
-if (!unref(page)) {
-	throw createError({ statusCode: 404, statusMessage: 'Page Not Found', fatal: true });
-}
+// if (!unref(page)) {
+// 	throw createError({ statusCode: 404, statusMessage: 'Page Not Found', fatal: true });
+// }
 
 const sections = computed(
 	() =>
@@ -86,7 +81,7 @@ useServerSeoMeta({
 
 useSchemaOrg([
 	defineWebPage({
-		url: `https://directus.io${path}`,
+		url: `https://directus.io/refer`,
 		name: unref(page)?.seo?.title ?? unref(page)?.title ?? undefined,
 		description: unref(page)?.seo?.meta_description ?? undefined,
 	}),
