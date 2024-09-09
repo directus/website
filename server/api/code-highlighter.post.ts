90ac5fe0167cd458658352b3949793aef58bcb12
bryantgillespie <hey@bryantgillespie.com>
2024-09-09 13:30:36 -0400

    Refactor code-highlighter.post.ts to use createHighlighter instead of getHighlighter

--- server/api/code-highlighter.post.ts
+++ server/api/code-highlighter.post.ts
@@ -1,5 +1,5 @@
 import type { ThemeInput } from 'shiki';
-import { getHighlighter } from 'shiki';
+import { createHighlighter } from 'shiki';
 import lightTheme from '~/assets/shiki/directus-light.json';
 import darkTheme from '~/assets/shiki/directus-dark.json';
 
@@ -9,17 +9,21 @@ interface CodeHighlighterBody {
 	background: string;
 }
 
+let shiki: any
+
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event);
+
+    if(!shiki) {
+        shiki = await createHighlighter({
+			themes: [lightTheme as unknown as ThemeInput, darkTheme as unknown as ThemeInput],
+			langs: ['graphql', 'http', 'typescript', 'sql', 'json'],
+		});
+    }
 
 	const { snippet, language, background } = body as CodeHighlighterBody;
 
 	try {
-		const shiki = await getHighlighter({
-			themes: [lightTheme as unknown as ThemeInput, darkTheme as unknown as ThemeInput],
-			langs: ['graphql', 'http', 'typescript', 'sql', 'json'],
-		});
-
 		const codeSnippet = shiki.codeToHtml(snippet, {
 			lang: language,
 			theme: background === 'dark-night' ? 'directus-dark' : 'directus-light',
