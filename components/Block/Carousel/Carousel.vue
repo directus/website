343233591421bed2b2884845639e9078f601dbf4
bryantgillespie <hey@bryantgillespie.com>
2024-11-12 11:05:50 -0500

    wip

--- components/Block/Carousel/Carousel.vue
+++ components/Block/Carousel/Carousel.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue';
-import emblaCarouselVue from 'embla-carousel-vue';
-import type { EmblaCarouselType } from 'embla-carousel';
-import CarouselCard from './CarouselCard.vue';
+import Carousel from '@/components/Carousel/Carousel.vue';
+import CarouselControls from '@/components/Carousel/CarouselControls.vue';
+import ClassNames from 'embla-carousel-class-names';
 
 const { $directus, $readItem } = useNuxtApp();
 const props = defineProps<{ uuid: string }>();
 
-const [emblaRef, emblaApi] = emblaCarouselVue({ loop: true });
 const block = ref<{ cards: Array<{ block_carousel_cards_id: string }> } | null>(null);
-const currentIndex = ref(0);
 
 const fetchSlides = async () => {
 	const result = await $directus.request(
@@ -21,101 +18,86 @@ const fetchSlides = async () => {
 	block.value = result as { cards: Array<{ block_carousel_cards_id: string }> };
 };
 
-const visibleSlides = computed(() => {
-	if (!block.value) return [];
-	const startIndex = currentIndex.value;
-	return block.value.cards.slice(startIndex, startIndex + 5);
-});
-
-const onNextClick = () => {
-	if (!block.value) return;
-	currentIndex.value = (currentIndex.value + 1) % block.value.cards.length;
-	updateSlideOpacity();
-};
-
-const onPrevClick = () => {
-	if (!block.value) return;
-	currentIndex.value = (currentIndex.value - 1 + block.value.cards.length) % block.value.cards.length;
-	updateSlideOpacity();
-};
-
-const updateSlideOpacity = () => {
-	if (!emblaApi.value) return;
-
-	const emblaSlides = emblaApi.value.slideNodes();
-	const slidesInView = emblaApi.value.slidesInView();
-
-	emblaSlides.forEach((slide, index) => {
-		slide.style.opacity = slidesInView.includes(index) ? '1' : '0.3';
-	});
-};
-
 onMounted(async () => {
 	await fetchSlides();
-
-	if (emblaApi.value) {
-		emblaApi.value.on('scroll', updateSlideOpacity);
-		emblaApi.value.on('reInit', updateSlideOpacity);
-		updateSlideOpacity();
-	}
 });
 </script>
 
 <template>
-	<div v-if="block" ref="emblaRef" class="embla">
-		<div class="embla__viewport">
-			<div class="embla__container">
-				<CarouselCard
-					v-for="(card, index) in visibleSlides"
-					:key="card.block_carousel_cards_id"
-					:uuid="card.block_carousel_cards_id"
-					:is-focused="index === 2"
-					class="embla__slide"
-				/>
-			</div>
-		</div>
-		<div class="embla__controls">
-			<BaseButton
-				color="secondary"
-				outline
-				class="embla__button embla__button--prev"
-				size="medium"
-				icon="arrow_back"
-				icon-position="right"
-				aria-label="Toggle navigation"
-				@click="onPrevClick"
-			/>
-			<BaseButton
-				color="secondary"
-				outline
-				class="embla__button embla__button--next"
-				size="medium"
-				icon="arrow_forward"
-				icon-position="right"
-				aria-label="Toggle navigation"
-				@click="onNextClick"
-			/>
-		</div>
+	<div v-if="block" class="embla">
+		<Carousel
+			:opts="{
+				loop: true,
+			}"
+			:plugins="[ClassNames({ snapped: 'slide--selected' })]"
+		>
+			<CarouselContent class="embla__container">
+				<CarouselItem v-for="card in block.cards" :key="card.block_carousel_cards_id" class="embla__slide">
+					<BlockCarouselCard :uuid="card.block_carousel_cards_id" />
+				</CarouselItem>
+			</CarouselContent>
+			<CarouselControls v-slot="{ onPrevClick, onNextClick }">
+				<div class="embla__controls">
+					<BaseButton
+						color="secondary"
+						outline
+						class="embla__button embla__button--prev"
+						size="large"
+						icon="arrow_back"
+						icon-position="right"
+						aria-label="Previous slide"
+						@click="onPrevClick"
+					/>
+					<BaseButton
+						color="secondary"
+						outline
+						class="embla__button embla__button--next"
+						size="large"
+						icon="arrow_forward"
+						icon-position="right"
+						aria-label="Next slide"
+						@click="onNextClick"
+					/>
+				</div>
+			</CarouselControls>
+		</Carousel>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 .embla {
 	margin: auto;
+	position: relative;
+
 	--slide-spacing: 40px;
-	--slide-size: 20%;
+	--slide-size: 40%;
 
 	&__container {
 		display: flex;
 		touch-action: pan-y pinch-zoom;
-		margin-left: calc(var(--slide-spacing) * -1);
+		// margin-left: calc(var(--slide-spacing) * -1);
 	}
 
 	&__slide {
+		position: relative;
+		transition-property: opacity, scale;
+		transition-timing-function: ease-in;
+		transition-duration: 0.5s;
 		transform: translate3d(0, 0, 0);
+		// width: 50%;
 		flex: 0 0 var(--slide-size);
-		min-width: 0;
-		padding-left: var(--slide-spacing);
+		// min-width: 0;
+		flex-shrink: 0;
+		flex-grow: 0;
+		// flex-basis: 40%;
+		// padding-left: var(--slide-spacing);
+		opacity: 0.5;
+		// scale: 0.9;
+
+		&.slide--selected {
+			opacity: 1;
+			// scale: 1.1;
+		}
 	}
 	&__controls {
 		display: flex;
---
+++ components/Carousel/CarouselControls.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCarousel } from './useCarousel';
+
+const { canScrollPrev, canScrollNext, scrollPrev, scrollNext } = useCarousel();
+</script>
+
+<template>
+	<div>
+		<slot
+			:can-scroll-prev="canScrollPrev"
+			:can-scroll-next="canScrollNext"
+			:on-prev-click="scrollPrev"
+			:on-next-click="scrollNext"
+		/>
+	</div>
+</template>
