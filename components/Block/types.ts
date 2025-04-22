11a342ef1a3752d3f3e927058030b508f975b872
Lindsey Zylstra <lindseyzylstra@gmail.com>
2025-04-22 12:25:34 -0700

    Remove extra type

--- components/Block/types.ts
+++ components/Block/types.ts
@@ -1,17 +1,3 @@
 export interface BlockProps {
 	uuid: string;
 }
-
-export interface BlockHeader {
-	id: string;
-	preheading: string | null;
-	heading: string | null;
-	subheading: string | null;
-	alignment: 'left' | 'center' | null;
-	heading_size: 'title' | 'x-large' | 'large' | 'medium' | 'small';
-	heading_tag: 'h1' | 'h2' | 'h3' | 'h4' | null;
-	button_group: string | null;
-	subheading_color: string | null;
-	subheading_type: string | null;
-	command: string | null;
-}
