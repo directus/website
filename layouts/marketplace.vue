0961310b6c697615102132df7e7640c0de27dc51
bryantgillespie <hey@bryantgillespie.com>
2025-07-08 22:31:03 -0400

    marektplace header and layout

---
+++ layouts/marketplace.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+	<ThemeProvider variant="light">
+		<NavHeader />
+		<MarketplaceHeader />
+		<slot />
+		<NavFooter />
+	</ThemeProvider>
+</template>
